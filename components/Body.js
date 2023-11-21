4ad8a8e30156fb367994848fad3a8870e57a0e9f
reddymanyam <reddappareddy18@gmail.com>
2023-11-21 20:23:45 +0530

    Body of app

--- components/Body.js
+++ components/Body.js
@@ -1,11 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
-import { useEffect, useState } from "react"; /* This is named export */
-import Shimmer from "./Shimmer"; /* This is default export */
+import { useEffect, useState } from "react"; 
+import Shimmer from "./Shimmer"; 
 import { swiggy_api_URL } from "../constants";
 import { Link } from "react-router-dom";
 
 
-// Filter the restaurant data according input type
 function filterData(searchText, restaurants) {
   const resFilterData = restaurants.filter((restaurant) =>
     restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
@@ -13,44 +12,44 @@ function filterData(searchText, restaurants) {
   return resFilterData;
 }
 
-// Body Component for body section: It contain all restaurant cards
+
 const Body = () => {
-  // useState: To create a state variable, searchText, allRestaurants and filteredRestaurants is local state variable
+  
   const [searchText, setSearchText] = useState("");
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  // use useEffect for one time call getRestaurants using empty dependency array
+  
   useEffect(() => {
     getRestaurants();
   }, []);
 
-  // async function getRestaurant to fetch Swiggy API data
+  
   async function getRestaurants() {
-    // handle the error using try... catch
+    
     try {
       const response = await fetch(swiggy_api_URL);
       const json = await response.json();
 
-      // initialize checkJsonData() function to check Swiggy Restaurant data
+     
       async function checkJsonData(jsonData) {
         for (let i = 0; i < jsonData?.data?.cards.length; i++) {
 
-          // initialize checkData for Swiggy Restaurant data
+          
           let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
-          // if checkData is not undefined then return it
+          
           if (checkData !== undefined) {
             return checkData;
           }
         }
       }
 
-      // call the checkJsonData() function which return Swiggy Restaurant data
+      
       const resData = await checkJsonData(json);
 
-      // update the state variable restaurants with Swiggy API data
+      
       setAllRestaurants(resData);
       setFilteredRestaurants(resData);
     } catch (error) {
@@ -58,7 +57,7 @@ const Body = () => {
     }
   }
 
-  // use searchData function and set condition if data is empty show error message
+  
   const searchData = (searchText, restaurants) => {
     if (searchText !== "") {
       const filteredData = filterData(searchText, restaurants);
@@ -73,7 +72,7 @@ const Body = () => {
     }
   };
 
-  // if allRestaurants is empty don't render restaurants cards
+  
   if (!allRestaurants) return null;
 
   return (
@@ -84,13 +83,13 @@ const Body = () => {
           className="search-input"
           placeholder="Search a restaurant you want..."
           value={searchText}
-          // update the state variable searchText when we typing in input box
+          
           onChange={(e) => setSearchText(e.target.value)}
         ></input>
         <button
           className="search-btn"
           onClick={() => {
-            // user click on button searchData function is called
+            
             searchData(searchText, allRestaurants);
           }}
         >
